69e0b202959dcf6b6571db1307a4f60e0e27db58
sjyong <seokjinyong@gamevilcom2us.com>
2022-07-21 20:20:06 +0900

    commit update - change name to unique

---
+++ YSJCustomFramework/YSJCustomFramework/public/YSJCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  YSJCustomFramework.h
+//  YSJCustomFramework
+//
+//  Created by sjyong on 2022/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YSJCustomFramework.
+FOUNDATION_EXPORT double YSJCustomFrameworkVersionNumber;
+
+//! Project version string for YSJCustomFramework.
+FOUNDATION_EXPORT const unsigned char YSJCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSJCustomFramework/PublicHeader.h>
+
+
